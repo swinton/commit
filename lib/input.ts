2e91979f1ad6301b86bc47be7ca10fdc69db9336
Steve Winton <stevewinton@gmail.com>
2020-12-08 12:15:09 -0600

    Allow input values to be null (when defaulted)

--- lib/input.ts
+++ lib/input.ts
@@ -2,10 +2,10 @@ import * as core from '@actions/core';
 
 export interface InputOptions extends core.InputOptions {
   /** Optional. Default value to use for an input option if the option is not set. */
-  default?: string
+  default?: string | null
 };
 
-export default function getInput(name: string, options: InputOptions = {}): string {
+export default function getInput(name: string, options: InputOptions = {}): string | null {
   const value = core.getInput(name, options);
 
   if (!value && options.default) {
