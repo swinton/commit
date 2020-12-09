740dc9b332d84857440d5212894e377cfe88e859
Steve Winton <stevewinton@gmail.com>
2020-12-09 15:26:25 -0600

    Prefer GH_TOKEN over GITHUB_TOKEN

--- lib/github-client.ts
+++ lib/github-client.ts
@@ -6,7 +6,7 @@ const github = axios.create({
   baseURL: `https://api.github.com/`,
   headers: {
     accept: `application/vnd.github.v3+json`,
-    authorization: `bearer ${ process.env.GITHUB_TOKEN }`,
+    authorization: `bearer ${ process.env.GH_TOKEN }`,
     'user-agent': `${ pkg.name }/${ pkg.version }`
   }
 });
