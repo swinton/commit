54cfd96d22767e2896f97cbea99b941fdc12aba8
Steve Winton <swinton@github.com>
2020-12-13 20:20:00 -0600

    Revert "Use pkginfo for package info"

--- lib/github-client.ts
+++ lib/github-client.ts
@@ -1,14 +1,15 @@
+import { join } from "path";
 import axios from "axios";
-import pkginfo from "pkginfo";
 
-pkginfo(module, "name", "version");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require(join(__dirname, "..", "package.json"));
 
 const github = axios.create({
   baseURL: `https://api.github.com/`,
   headers: {
     accept: `application/vnd.github.v3+json`,
     authorization: `bearer ${process.env.GH_TOKEN}`,
-    "user-agent": `${module.exports.name}/${module.exports.version}`,
+    "user-agent": `${pkg.name}/${pkg.version}`,
   },
 });
 
