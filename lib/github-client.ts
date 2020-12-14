c903894680f948840378df2760d2098efc4ff7bc
Steve Winton <stevewinton@gmail.com>
2020-12-13 20:13:33 -0600

    Use pkginfo for package info

--- lib/github-client.ts
+++ lib/github-client.ts
@@ -1,15 +1,14 @@
-import { join } from "path";
 import axios from "axios";
+import pkginfo from "pkginfo";
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require(join(__dirname, "..", "package.json"));
+pkginfo(module, "name", "version");
 
 const github = axios.create({
   baseURL: `https://api.github.com/`,
   headers: {
     accept: `application/vnd.github.v3+json`,
     authorization: `bearer ${process.env.GH_TOKEN}`,
-    "user-agent": `${pkg.name}/${pkg.version}`,
+    "user-agent": `${module.exports.name}/${module.exports.version}`,
   },
 });
 
